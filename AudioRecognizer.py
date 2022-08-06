aa3a25690b1ccd84b7fcd195269d0b9be7276873
Atifbeast <91662836+Atifbeast@users.noreply.github.com>
2022-08-06 17:27:51 +0530

    Update AudioRecognizer.py

--- AudioRecognizer.py
+++ AudioRecognizer.py
@@ -87,11 +87,13 @@ def stream():
     st.write(current_dir)
     uploading_path = os.path.join(current_dir, directory_name)
     
-    st.write("WITHOUT FILE.WAV PATH : ", os.listdir(uploading_path))
+#     st.write("WITHOUT FILE.WAV PATH : ", os.listdir(uploading_path))
     
     for i in uploaded_file:
         names = i.name 
         full_path_file_name = os.path.join(uploading_path, names)
+        full_path_file_name = os.path.abspath(full_path_file_name)
+        st.write("ABSOLUTE PATH : ", full_path_file_name)
         st.write("ALL FILES INSIDE PATH : ", os.listdir(full_path_file_name))
         st.write(f"YOU SELECTED : {names}")
         st.write("FILE : ", full_path_file_name)
