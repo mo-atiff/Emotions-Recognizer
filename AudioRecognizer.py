2683019837f2ec9ce044369e5ee3ad72a0ac2b98
Atifbeast <91662836+Atifbeast@users.noreply.github.com>
2022-08-14 16:41:57 +0530

    Update AudioRecognizer.py

--- AudioRecognizer.py
+++ AudioRecognizer.py
@@ -28,11 +28,11 @@ st.set_page_config(
     page_icon="🔊",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+#     menu_items={
+#         'Get Help': 'https://www.extremelycoolapp.com/help',
+#         'Report a bug': "https://www.extremelycoolapp.com/bug",
+#         'About': "# This is a header. This is an *extremely* cool app!"
+#     }
 )
 
 
